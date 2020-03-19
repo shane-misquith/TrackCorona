366d3c296e3f1e90f00d5f7061f811228386b282
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-03-19 12:05:06 +0530

    added index.html in the proper format

--- TrackCorona/urls.py
+++ TrackCorona/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from mainapp import views,urls
 
 urlpatterns = [
+    url(r'^$',views.index,name="index"),
     path('admin/', admin.site.urls),
+    url(r'^mainapp/',include('mainapp.urls')),
 ]
---
+++ mainapp/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from mainapp import views
+
+#template tagging
+app_name = 'mainapp'
+urlpatterns=[
+    # url(r'^clientsignuppage/$',views.signuppage,name='signUpPage'),
+    # url(r'^lawyersignuppage/$',views.lawsignuppage,name='LawSignUp'),
+    # url(r'^signinpage/$',views.signinpage,name='signIn'),
+    # url(r'^userWelcome/$',views.userWelcome,name='userWelcome'),
+    # url(r'^help/$',views.helppage,name='helpPage'),
+]
